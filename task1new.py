84eca0e532397799fdc300c81af36c4880738863
Ilya Zatsepin <ilyadobrygreen@gmail.com>
2025-09-23 09:11:42 +0300

    Updated_1

--- task1new.py
+++ task1new.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 trans_fi = lambda x: np.e**(0.5*(1-(np.log10(x))**2))-1
 trans = lambda x: 3*(np.log10(x))**2+6*np.log(x+1)-3 
 algebraic = lambda x: x**4-3*x**3+3*x**2-2
@@ -17,11 +16,9 @@ class Solvers:
         c = (a+b)/2
         i = 0
         err = []
-        x = []
         while abs(b-a)>2*eps and i < max_iter:
             i += 1
             c = (a+b)/2
-            x.append(c)
             err.append(abs(c-tr))
             if f(a)*f(c)<0:
                 b = c
@@ -29,37 +26,12 @@ class Solvers:
                 break
             else:
                 a = c
+        err.append(abs((a+b)/2-tr))
         self.root = float((a+b)/2)
         self.i = i
         self.err = err
 
-    def wegsteins_method_orig(self,x0,tr,eps,f,max_iter = 1000):
-        self.name = "Wegstein's method"
-        i = 0
-        err = []
-        x_n = x0
-        while i< max_iter:
-            i += 1
-            if i == 1:
-                xnext = f(x_n)
-                x_prev = x0
-                x_n = xnext
-                xn = xnext
-                continue
-            xnext = f(x_n)
-            p = (xnext-xn)/(x_n-x_prev)
-            q = p/(p-1)
-            x_next = q*x_n + (1-q)*xnext
-            err.append(abs(float(tr-x_next)))
-            if abs(x_next-x_n)<eps and abs(x_next - f(x_next))<eps:
-                break
-            x_prev = x_n
-            x_n = x_next
-            xn = xnext
-        self.root = float(x_next)
-        self.i = i
-        self.err = err
-    def wegsteins_method(self,x0,tr,eps,f,max_iter = 1000):
+    def wegsteins_method(self,x0,tr,eps,f,unfi,max_iter = 1000):
         self.name = "Wegstein's method"
         i = 0
         err = []
@@ -75,7 +47,7 @@ class Solvers:
             xnext = f(x_n)
             x_next = (xnext*x_prev-xn*x_n)/(xnext+x_prev-xn-x_n)
             err.append(abs(float(tr-x_next)))
-            if abs(x_next-x_n)<eps :
+            if unfi(x_next-eps)*unfi(x_next+eps)<0 :
                 break
             x_prev = x_n
             x_n = x_next
@@ -133,11 +105,11 @@ class Plotters:
         plt.show()
     def convergence_2(self,a,b,tr,eps,f,phi):
         solver = Solvers()
-        solver.wegsteins_method((a+b)/2,tr,eps,phi)
+        solver.wegsteins_method((a+b)/2,tr,eps,phi,f)
         x1 = [i for i in range(solver.i-1)]
         y1 = solver.err
         solver.bisection_method(a,b,tr,eps,f)
-        x2 = [i for i in range(solver.i)]
+        x2 = [i for i in range(solver.i+1)]
         y2 = solver.err
         plt.plot(x1,y1,'-o',color='red',label = 'Wegstein')
         plt.plot(x2,y2,'-*',label = 'Bisection')
@@ -181,7 +153,7 @@ class Plotters:
         i_list1 = [] 
         i_list2 = []
         for eps in eps_list:
-                solver.wegsteins_method((a+b)/2,tr,eps,phi)
+                solver.wegsteins_method((a+b)/2,tr,eps,phi,f)
                 i_list1.append(solver.i)
         for eps in eps_list:
                 solver.bisection_method(a,b,tr,eps,f)
@@ -235,7 +207,7 @@ class Plotters:
         err_list1 = []
         err_list2 = []
         for eps in eps_list:
-            solver.wegsteins_method((a+b)/2,tr,eps,phi)
+            solver.wegsteins_method((a+b)/2,tr,eps,phi,f)
             if len(solver.err)>=1:
                 err_list1.append(solver.err[-1])
         y1 = err_list1
@@ -257,18 +229,23 @@ class Plotters:
         plt.show()
     
     
-    def initial_guess_effect(self,method,a,b,eps,f,tr=0):
+    def initial_guess_effect(self,method,a,b,eps,fi,f,tr=-1000):
         solver = Solvers()
         x_list = np.linspace(a,b,100)
         i_list = []
         if method == 'Wegstein':
             for x in x_list:
-                solver.wegsteins_method(x,tr,eps,f)
+                solver.wegsteins_method(x,tr,eps,fi,f)
                 i_list.append(solver.i)
         else:
             print('Choose a valid method name')
         y = i_list
-        plt.plot(x_list,y,'-o') 
+        
+        plt.plot(x_list,y) 
+        if tr != -1000:
+            plt.axvline(tr,linestyle = 'dashed')
+            yy = plt.ylim()
+            plt.annotate(f'$x^*$ = {tr}',[tr,min(yy)],[tr,min(yy)-0.2])
         plt.grid(True)
         plt.xlabel('$x_0$')
         plt.ylabel('Iterations')
@@ -279,25 +256,25 @@ solver2 = Solvers()
 plotter = Plotters()
 solver1.bisection_method(1,2,0,1e-6,algebraic)
 print(round(solver1.root,7))
-solver1.wegsteins_method(1.5,0,1e-6,algebraic_fi)
+solver1.wegsteins_method(1.5,0,1e-6,algebraic_fi,algebraic)
 print(round(solver1.root,7))
 solver2.bisection_method(0.4,1,0,1e-6,trans)
 print(round(solver2.root,7))
-solver2.wegsteins_method(1.5,0,1e-6,trans_fi)
+solver2.wegsteins_method(1.5,0,1e-6,trans_fi,trans)
 print(round(solver2.root,7))
 # ###Algebraic
 
 
-#Convergence
+# #Convergence
 # plotter.convergence_2(0.4,5,0.61154552014579682218,1e-6,trans,trans_fi)
 # plotter.convergence_2(1,2,1.61803398874989,1e-6,algebraic,algebraic_fi)
-#Computational_difficulty
-plotter.computational_difficulty_2(0.4,1,trans,trans_fi)
+# #Computational_difficulty
+# plotter.computational_difficulty_2(0.4,1,trans,trans_fi)
 # plotter.computational_difficulty_2(1,2,algebraic,algebraic_fi)
-# #Accuracy
+# # #Accuracy
 # plotter.accuracy_2(0.4,1,0.61154552014579682218,trans,trans_fi)
 # plotter.accuracy_2(1,2,1.61803398874989,algebraic,algebraic_fi)
-# #Initial_guess_effect
+# # #Initial_guess_effect
 
-plotter.initial_guess_effect('Wegstein',0.4,1,1e-6,trans_fi)
-plotter.initial_guess_effect('Wegstein',1,2,1e-6,algebraic_fi)
+plotter.initial_guess_effect('Wegstein',0.4,1,1e-6,trans_fi,trans,tr = 0.6115458)
+plotter.initial_guess_effect('Wegstein',1,2,1e-6,algebraic_fi,algebraic,tr = 1.6180334)
